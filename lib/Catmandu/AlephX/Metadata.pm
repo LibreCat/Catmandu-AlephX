c353bcdfd11303d5db07ae1d8d86b2da1498b493
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2018-01-24 14:57:01 +0100

    Adding Dist::Milla files and file versions

--- lib/Catmandu/AlephX/Metadata.pm
+++ lib/Catmandu/AlephX/Metadata.pm
@@ -2,6 +2,8 @@ package Catmandu::AlephX::Metadata;
 use Catmandu::Sane;
 use Moo;
 
+our $VERSION = "1.065";
+
 has type => (is => 'ro',required => 1);
 has data => (is => 'ro',required => 1);
 
--- lib/Catmandu/AlephX/Record/Present.pm
+++ lib/Catmandu/AlephX/Record/Present.pm
@@ -2,6 +2,8 @@ package Catmandu::AlephX::Record::Present;
 use Catmandu::Sane;
 use Moo;
 
+our $VERSION = "1.065";
+
 extends 'Catmandu::AlephX::Record';
 
 has record_header => (is => 'ro',required => 1);
